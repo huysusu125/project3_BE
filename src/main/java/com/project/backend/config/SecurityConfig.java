b6af3de558f455bb2bf2efdc99b54b13a86538bb
huytd <huy.td173171@sis.hust.edu.vn>
2021-12-22 22:40:59 +0700

    1

--- src/main/java/com/project/backend/config/SecurityConfig.java
+++ src/main/java/com/project/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.backend.config;
 
+import com.project.backend.constain.RoleName;
 import com.project.backend.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -27,10 +28,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests()
+                .antMatchers("/api/login/**").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
+                .anyRequest().authenticated(); // các địa chỉ khác phải login mới được
+        http.authorizeRequests()
+                .antMatchers("/api/users/**").hasAnyAuthority(RoleName.USER)
+                .antMatchers("api/ownerLocation").hasAnyAuthority(RoleName.OWNER_LOCATION, RoleName.USER);
+
+//        http.authorizeRequests().anyRequest().permitAll(); // cho phép tất cả
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
